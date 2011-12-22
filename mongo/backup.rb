6c579031ea31ea4bb625fd8baa86f8ef9690959a
Jules Bovet <jules.bovet@gmail.com>
2011-12-22 16:44:36 +0100

    Add mongodb backup to s3

--- mongo/backup.rb
+++ mongo/backup.rb
@@ -1,39 +1,49 @@
 require 'aws/s3'
 require 'yaml'
 
-BACKUP_NUMBER=300
+# Store mongodump objects to amazon s3
+#
+# Amazon s3 configuration file must be named amazon_s3.yml in $HOME
+# or AMAZON_S3_PATH must be set in the environment
 
-def run_recursively(dir, base)
+def run_recursively(list, key_list, dir, base)
   Dir.foreach(dir) do |entry|
-    if entry[0] != '.' && File.directory?(entry)
-      run_recursively(dir + '/' + entry, base + '/' + entry)
-    else
-      file = base + '/' + entry
-      S3Object.store(file, open(file), backup_bucket)
+    if entry.length > 1 && entry[0] != '.'
+      if File.directory?(dir + '/' + entry)
+        run_recursively(list, key_list, dir + '/' + entry, base + '/' + entry)
+      else
+        file = base + '/' + entry
+        count = key_list.count(file)
+        puts "Storing file #{file}... (#{count} already)"
+        AWS::S3::S3Object.store(file + '-' + Time.now.to_s[0..-7], open(dir + '/' + entry), BACKUP_BUCKET)
+      end
     end
   end
 end
 
 # Load configuration file
-S3_CONFIG = YAML.load_file(ENV['HOME'] + '/amazon_s3.yml')
+s3_path = ENV['AMAZON_S3_PATH'] || ENV['HOME'] + '/amazon_s3.yml'
+S3_CONFIG = YAML.load_file(s3_path)
 
-backup_bucket = S3_CONFIG['backup_bucket']
+BACKUP_BUCKET = S3_CONFIG['backup_bucket']
+
 dump_dir = S3_CONFIG['dump_directory']
-base_dir = dump_dir.split('/')[-1]
-
-if !dump_dir
-  dump_dir = 'dump-tmp'
-end
 
 %x[mkdir -p #{dump_dir}; cd #{dump_dir}; mongodump]
 
+# Fix to make aws/s3 works with european buckets
+AWS::S3::DEFAULT_HOST.replace "s3-eu-west-1.amazonaws.com"
 AWS::S3::Base.establish_connection!(
     :access_key_id     => S3_CONFIG['access_key_id'],
     :secret_access_key => S3_CONFIG['secret_access_key']
   )
 
-Dir.chdir(dump_dir) do
-  run_recursively(Dir.pwd, base_dir)
+list = AWS::S3::Bucket.objects(BACKUP_BUCKET)
+key_list = list.map { |x| x.key().split('-')[0] }
+
+Dir.chdir(dump_dir + '/dump') do
+  run_recursively(list, key_list, Dir.pwd, Dir.pwd.split('/')[-1])
+  puts "Upload done !"
 end
 
 %x[rm -rf #{dump_dir}]
