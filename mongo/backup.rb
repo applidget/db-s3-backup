af2236d563b58bf64a3cfd0249a897ec54bb459f
Jules Bovet <jules.bovet@gmail.com>
2011-12-27 10:08:22 +0100

    Add Gemfile and a nice output when running the script

--- mongo/backup.rb
+++ mongo/backup.rb
@@ -1,35 +1,39 @@
 require 'aws/s3'
 require 'yaml'
 
-# Store mongodump objects to amazon s3
+# Store mongodump objects to amazon s3. Database must be used as a replica set
 #
 # Amazon s3 configuration file must be named amazon_s3.yml in $HOME
 # or AMAZON_S3_PATH must be set in the environment
+#
+BACKUP_NB = 100
 
-def run_recursively(list, key_list, dir, base)
-  Dir.foreach(dir) do |entry|
-    if entry.length > 1 && entry[0] != '.'
-      if File.directory?(dir + '/' + entry)
-        run_recursively(list, key_list, dir + '/' + entry, base + '/' + entry)
-      else
-        file = base + '/' + entry
-        count = key_list.count(file)
-        puts "Storing file #{file}... (#{count} already)"
-        AWS::S3::S3Object.store(file + '-' + Time.now.to_s[0..-7], open(dir + '/' + entry), BACKUP_BUCKET)
-      end
+# String format is dump-YYYY-MM-DD_HH:MM:SS.tar.gz
+def get_time_object(str)
+  obj = Time.new(str[5..8].to_i, str[10..11].to_i, str[13..14].to_i,
+                 str[16..17].to_i, str[19..20].to_i, str[22..23].to_i)
+  obj
+end
+
+# Return position of the oldest archive (to delete)
+def get_older_file(list)
+  tmp = Time.now
+  name = ''
+
+  list.each do |elem|
+    elem_to_time = get_time_object(elem)
+    if elem_to_time < tmp
+      tmp = elem_to_time
+      name = elem
     end
   end
+
+  list.index(name)
 end
 
 # Load configuration file
 s3_path = ENV['AMAZON_S3_PATH'] || ENV['HOME'] + '/amazon_s3.yml'
 S3_CONFIG = YAML.load_file(s3_path)
-
-BACKUP_BUCKET = S3_CONFIG['backup_bucket']
-
-dump_dir = S3_CONFIG['dump_directory']
-
-%x[mkdir -p #{dump_dir}; cd #{dump_dir}; mongodump]
 
 # Fix to make aws/s3 works with european buckets
 AWS::S3::DEFAULT_HOST.replace "s3-eu-west-1.amazonaws.com"
@@ -38,12 +42,39 @@ AWS::S3::Base.establish_connection!(
     :secret_access_key => S3_CONFIG['secret_access_key']
   )
 
-list = AWS::S3::Bucket.objects(BACKUP_BUCKET)
-key_list = list.map { |x| x.key().split('-')[0] }
+# Get s3 backup bucket and dump directory
+BACKUP_BUCKET = S3_CONFIG['backup_bucket']
+dump_dir = S3_CONFIG['dump_directory']
 
-Dir.chdir(dump_dir + '/dump') do
-  run_recursively(list, key_list, Dir.pwd, Dir.pwd.split('/')[-1])
-  puts "Upload done !"
+# Create dump directory
+puts "MongoDB Backup started !"
+puts "----> Dumping data"
+puts "      Creating dump directory #{dump_dir}"
+puts "      Running mongodump..."
+%x[mkdir -p #{dump_dir}; cd #{dump_dir}; mongodump >/dev/null]
+
+# Get object list from our bucket
+puts "----> Getting object list"
+list = AWS::S3::Bucket.objects(BACKUP_BUCKET)
+
+# Delete oldest object if we have to many backups
+if list.length >= BACKUP_NB
+  puts "----> Deleting oldest backup"
+  AWS::S3::S3Object.delete(list[get_older_file(list)], BACKUP_BUCKET)
 end
 
+# Go to the dump directory, make the tarball and upload it
+Dir.chdir(dump_dir) do
+  puts "----> Save backup to S3"
+  tarball = "dump-#{Time.now.to_s[0..-7]}.tar.gz"
+  tarball.gsub!(' ', '_')
+  puts "      Creating tarball `#{tarball}'"
+  %x[tar -cf #{tarball} dump]
+  puts "      Storing tarball to bucket `#{BACKUP_BUCKET}'"
+  AWS::S3::S3Object.store(tarball, open(tarball), BACKUP_BUCKET)
+end
+
+# Delete dump directory we just created
+puts "----> Cleaning-up"
 %x[rm -rf #{dump_dir}]
+puts "Backup done !"
